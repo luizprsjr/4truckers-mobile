5d45a7ba7cde7cfe15f9ec383f636f0428be866e
Luiz Pires <lpiresjunior@gmail.com>
2023-09-08 02:28:41 -0300

    feat: return the loading component if user storage data is loading

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { View } from 'react-native'
 
+import { Loading } from '@components/Loading'
 import { useAuth } from '@hooks/useAuth'
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native'
 import { colors } from '@theme/index'
@@ -8,10 +9,12 @@ import { AppRoutes } from './app.routes'
 import { AuthRoutes } from './auth.routes'
 
 export function Routes() {
-  const { user } = useAuth()
+  const { user, isLoadingUserStorageData } = useAuth()
 
   const theme = DefaultTheme
   theme.colors.background = colors.secondary50
+
+  if (isLoadingUserStorageData) return <Loading />
 
   return (
     <View style={{ flex: 1, backgroundColor: colors.secondary50 }}>
