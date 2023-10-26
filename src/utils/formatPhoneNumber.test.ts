bd9bffd1813b64114568939d30e6b257f8138502
Luiz Pires <lpiresjunior@gmail.com>
2023-10-26 12:38:41 -0300

    test: formatPhoneNumber

---
+++ src/utils/formatPhoneNumber.test.ts
@@ -0,0 +1,13 @@
+import { formatPhoneNumber } from './formatPhoneNumber'
+
+describe('function: formatPhoneNumber', () => {
+  it('should format a valid phone number string to the (99) 9 9999-9999 format', () => {
+    const formattedNumber = formatPhoneNumber('99999999999')
+    expect(formattedNumber).toBe('(99) 9 9999-9999')
+  })
+
+  it('should return false if the input string is not a valid phone number', () => {
+    const formattedNumber = formatPhoneNumber('xxxxxxxxxxx')
+    expect(formattedNumber).toBeFalsy()
+  })
+})
