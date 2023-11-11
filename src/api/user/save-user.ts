0f421ce7b9162c8375b25279fe99e47ae324cc44
Luiz Pires <lpiresjunior@gmail.com>
2023-11-11 18:02:15 -0300

    feat: create save user hook

---
+++ src/api/user/save-user.ts
@@ -0,0 +1,46 @@
+import { UserDTO } from '@dtos/UserDTO'
+import { UpdateUserFormData } from '@screens/profile'
+import { useMutation } from '@tanstack/react-query'
+
+import { api } from '../'
+
+type Response = {
+  user: UserDTO
+}
+
+type Params = {
+  formData: UpdateUserFormData
+  user: UserDTO
+}
+
+export async function saveUser({ formData, user }: Params): Promise<UserDTO> {
+  const newInfos = {
+    name: formData.name || user.name,
+    phoneNumber: formData.phoneNumber
+      ? formData.phoneNumber.replace(/\D/g, '')
+      : user.phoneNumber,
+    truckModel: formData.truckModel
+      ? formData.truckModel
+      : user.truck
+      ? user.truck.truckModel
+      : undefined,
+    capacity: formData.capacity ? Number(formData.capacity) : undefined,
+    length: formData.length ? Number(formData.length) : undefined,
+    width: formData.width ? Number(formData.width) : undefined,
+    height: formData.height ? Number(formData.height) : undefined,
+  }
+
+  const { data } = await api.put<Response>('/users', newInfos)
+
+  if (!data || !data.user) {
+    throw new Error('Invalid API response format')
+  }
+
+  return data.user
+}
+
+export function useSaveUser() {
+  return useMutation({
+    mutationFn: saveUser,
+  })
+}
